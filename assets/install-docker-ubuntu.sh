7b11621218798808263e12f81737e76d3e817190
Anatoly <politsin@gmail.com>
2017-09-06 21:06:30 +0300

    add git

--- assets/install-docker-ubuntu.sh
+++ assets/install-docker-ubuntu.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -qq apt-transport-https ca-certificates curl software-properties-common htop -y
+apt-get install -qq apt-transport-https ca-certificates curl software-properties-common htop git -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-apt-get update
+apt-get update && \
 apt-get install docker-ce -y
 curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
